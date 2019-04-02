6ecec18b96d36f51e12638cc8e189b9dcff342ba
Villads V K Nielsen <villadsvalur@gmail.com>
2019-04-02 13:43:03 +0200

    Tilføj product model og controller(#2)
    * Initial commit, again
    
    * add ClientApp
    
    * added toolsversion and sln file
    
    * added a product-controller and model

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using pluk13_web.Models;
+
+namespace pluk13_web.Controllers
+{
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        [HttpGet("{base}productList")]
+        public List<Product> GetAllProducts()
+        {
+            List<Product> productsList = new List<Product>();
+
+
+            productsList.Add(new Product(1, "Mors Varme Boller", "A-1-2"));
+
+
+            return productsList;
+        }
+
+        [HttpGet("[action]")]
+        public Product GetProduct(int id)
+        {
+            //open db connect
+            //select statement: select * from Products where ProductId = id
+            //nyt product med info fra db:
+            Product product = new Product(2, "Fars Kolde Boller", "B-2-3");
+            return product;
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace pluk13_web.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public string Brand { get; set; }
+        public string OtherInfo { get; set; }
+        public string Location { get; set; }
+
+        public Product(int id, string title, string location)
+        {
+            ProductId = id;
+            Title = title;
+            Location = location;
+        }
+    }
+}
