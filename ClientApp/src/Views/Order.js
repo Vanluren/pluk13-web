9e918aa35e588d52f315ae72b98ee295c07af17b
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2019-05-16 01:42:34 +0200

    finished the order view

--- ClientApp/src/Views/Order.js
+++ ClientApp/src/Views/Order.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Container, Row, Col } from "reactstrap";
+import { Divider, AppBar, Toolbar, IconButton, Paper } from "@material-ui/core";
 import {
-  Divider,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Paper,
-  List,
-  ListItem,
-  ListItemText
-} from "@material-ui/core";
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableTreeColumn,
+  TableRowDetail
+} from "@devexpress/dx-react-grid-material-ui";
+import {
+  TreeDataState,
+  CustomTreeData,
+  RowDetailState
+} from "@devexpress/dx-react-grid";
 import { Print } from "@material-ui/icons/";
 
 class Order extends Component {
@@ -26,7 +29,16 @@ class Order extends Component {
     const { currentOrder } = this.state;
     fetch(`api/orders/${currentOrder}`)
       .then(res => res.json())
-      .then(res => this.setState({ orderInfo: res }))
+      .then(res => {
+        return this.setState({
+          orderInfo: res,
+          expandedProducts: res.products.map(p => p.productId),
+          expandedGifts: Array.from(
+            Array(res.gifts.length * 10),
+            (x, idx) => idx++
+          )
+        });
+      })
       .then(() => {
         if (location.state && location.state.print) {
           window.print();
@@ -34,47 +46,19 @@ class Order extends Component {
       });
   }
 
-  renderGifts = () => {
-    const { orderInfo } = this.state;
-    if (orderInfo.gifts) {
-      return orderInfo.gifts.map(gift => {
-        const content = gift.contents.map(p => p.productTitle).join(",");
-        const secondary = `
-        Id: ${gift.giftId}, \n
-        Indhold: "${content}"
-        `;
-        return (
-          <ListItem key={gift.giftId}>
-            <ListItemText
-              primary={`${gift.giftQuantity} x ${gift.giftTitle}`}
-              secondary={secondary}
-            />
-          </ListItem>
-        );
-      });
-    }
+  RowDetail = ({ row }) => <div>{row.otherInfo}</div>;
+  getChildRows = (row, rootRows) => {
+    this.setState({ expandedGifts: this.state.expandedGifts.push(row) });
+    return row ? row.contents : rootRows;
   };
-  renderProducts = () => {
-    const { orderInfo } = this.state;
-    if (orderInfo.products) {
-      return orderInfo.products.map(prod => {
-        const secondary = `
-        Id: ${prod.productId}, \n
-        Location: "${prod.location}"
-        `;
-        return (
-          <ListItem key={prod.productId}>
-            <ListItemText
-              primary={`${prod.productQuantity} x ${prod.productTitle}`}
-              secondary={secondary}
-            />
-          </ListItem>
-        );
-      });
-    }
-  };
+
   render() {
-    const { currentOrder, orderInfo } = this.state;
+    const {
+      currentOrder,
+      orderInfo,
+      expandedGifts,
+      expandedProducts
+    } = this.state;
     return (
       <Container>
         <Row>
@@ -105,7 +89,79 @@ class Order extends Component {
                   <Col>
                     <ListHeader>Gaver:</ListHeader>
                     <Divider variant="middle" />
-                    <List>{this.renderGifts()}</List>
+                    <Grid
+                      columns={[
+                        {
+                          name: "giftId",
+                          disablePadding: false,
+                          title: "Gave Id"
+                        },
+                        {
+                          name: "giftTitle",
+                          disablePadding: false,
+                          title: "Gave Navn"
+                        },
+                        {
+                          name: "productId",
+                          disablePadding: false,
+                          title: "Id"
+                        },
+                        {
+                          name: "productTitle",
+                          disablePadding: false,
+                          title: "Titel"
+                        },
+                        {
+                          name: "size",
+                          disablePadding: false,
+                          title: "Størrelse"
+                        },
+                        {
+                          name: "brand",
+                          disablePadding: false,
+                          title: "Mærke"
+                        },
+                        {
+                          name: "productQuantity",
+                          disablePadding: false,
+                          title: "Antal"
+                        },
+                        {
+                          name: "location",
+                          disablePadding: false,
+                          title: "Placering"
+                        }
+                      ]}
+                      rows={orderInfo.gifts}
+                    >
+                      <TreeDataState defaultExpandedRowIds={expandedGifts} />
+                      <CustomTreeData getChildRows={this.getChildRows} />
+                      <Table
+                        columnExtensions={[
+                          { columnName: "giftId", width: 135 },
+                          {
+                            columnName: "giftTitle",
+                            width: 210,
+                            wordWrapEnabled: true
+                          },
+                          { columnName: "productId", width: 100 },
+                          {
+                            columnName: "productTitle",
+                            width: 200,
+                            wordWrapEnabled: true
+                          },
+                          { columnName: "size", width: 100 },
+                          {
+                            columnName: "brand",
+                            width: 120,
+                            wordWrapEnabled: true
+                          },
+                          { columnName: "productQuantity", width: 50 }
+                        ]}
+                      />
+                      <TableHeaderRow />
+                      <TableTreeColumn for="giftId" />
+                    </Grid>
                   </Col>
                 </Row>
               )}
@@ -114,7 +170,73 @@ class Order extends Component {
                   <Col>
                     <ListHeader>Produkter:</ListHeader>
                     <Divider variant="middle" />
-                    <List>{this.renderProducts()}</List>
+                    <Grid
+                      columns={[
+                        {
+                          name: "productId",
+                          disablePadding: false,
+                          title: "Id"
+                        },
+                        {
+                          name: "productTitle",
+                          disablePadding: false,
+                          title: "Titel"
+                        },
+                        {
+                          name: "size",
+                          disablePadding: false,
+                          title: "Størrelse"
+                        },
+                        {
+                          name: "brand",
+                          disablePadding: false,
+                          title: "Mærke"
+                        },
+                        {
+                          name: "otherInfo",
+                          disablePadding: false,
+                          title: "Info"
+                        },
+                        {
+                          name: "productQuantity",
+                          disablePadding: false,
+                          title: "Antal"
+                        },
+                        {
+                          name: "location",
+                          disablePadding: false,
+                          title: "Placering"
+                        }
+                      ]}
+                      rows={orderInfo.products}
+                      getRowId={row => row.productId}
+                    >
+                      <RowDetailState expandedRowIds={expandedProducts} />
+                      <Table
+                        columnExtensions={[
+                          { columnName: "productId", width: 100 },
+                          {
+                            columnName: "productTitle",
+                            width: 250,
+                            wordWrapEnabled: true
+                          },
+                          { columnName: "size", width: 130 },
+                          {
+                            columnName: "brand",
+                            width: 120,
+                            wordWrapEnabled: true
+                          },
+                          {
+                            columnName: "otherInfo",
+                            width: 215,
+                            wordWrapEnabled: true
+                          },
+                          { columnName: "productQuantity", width: 50 }
+                        ]}
+                      />
+                      <TableHeaderRow />
+                      <TableRowDetail contentComponent={this.RowDetail} />
+                    </Grid>
                   </Col>
                 </Row>
               )}
@@ -128,7 +250,9 @@ class Order extends Component {
 
 const ListHeader = styled.div`
   font-size: 25px;
-  padding: 15px;
+  /* jeg ved ikke hvorfor det næste virker, det gør det bare. */
+  padding: 13px 0px;
+  margin: 15px;
 `;
 
 export default Order;
