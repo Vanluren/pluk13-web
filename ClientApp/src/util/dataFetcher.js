e7a88c03311aa5b636e8b3e6271a63fa563601e0
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2019-09-17 11:08:27 +0200

    fixed api req urls

--- ClientApp/src/util/dataFetcher.js
+++ ClientApp/src/util/dataFetcher.js
@@ -1,6 +1,6 @@
 export const getGifts = () =>
-  fetch("//localhost:5001/api/gifts").then(response => response.json());
+  fetch("/api/gifts").then(response => response.json());
 export const getProducts = () =>
-  fetch("//localhost:5001/api/products").then(response => response.json());
+  fetch("/api/products").then(response => response.json());
 export const getOrders = () =>
-  fetch("//localhost:5001/api/orders").then(response => response.json());
+  fetch("/api/orders").then(response => response.json());
